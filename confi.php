11e53307dffc8769c90526162003ae6f25120d24
Saiful Omar <omar.saiful@gmail.com>
2015-04-23 19:04:20 +0800

    confi commit

--- confi.php
+++ confi.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysql_connect("localhost", "admin8ebqDpM", "jzVcbRM-7Yas");
+$conn = mysql_connect("127.3.9.130:3306", "adminiWrVJlR", "7WPcJIjiiwGd");
 mysql_select_db('itb_notification', $conn);
 ?>
