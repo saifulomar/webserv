22ef881b55baf6e8fbe9bc93a304eaab9a79c614
Saiful Omar <omar.saiful@gmail.com>
2015-04-23 19:46:13 +0800

    confi commit3

--- confi.php
+++ confi.php
@@ -1,4 +1,8 @@
 <?php
 $conn = mysql_connect("127.3.9.130", "adminiWrVJlR", "7WPcJIjiiwGd");
 mysql_select_db('itb_notification', $conn);
+if (!$conn) {
+    die('Could not connect: ' . mysql_error());
+}
+printf("MySQL server version: %s\n", mysql_get_server_info());
 ?>
